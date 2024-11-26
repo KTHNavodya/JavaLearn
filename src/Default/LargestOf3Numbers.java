d30748e500affc5cc176ada55d9035e36f012184
KTHNavodya <navodya43hiruni@gmail.com>
2024-11-26 09:48:26 +0530

    FindLargestNumber

---
+++ src/Default/LargestOf3Numbers.java
@@ -0,0 +1,36 @@
+package Default;
+
+import java.util.Scanner;
+
+public class LargestOf3Numbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter First Number:");
+        int a=sc.nextInt();
+
+        System.out.println("Enter Second Number:");
+        int b=sc.nextInt();
+
+        System.out.println("Enter Third Number:");
+        int c=sc.nextInt();
+
+        //Appraoch1 - Logic
+
+        if(a>b && a>c)
+        {
+            System.out.println(a+ "is Largest Number");
+        }
+        else if(b>a && b>c)
+        {
+            System.out.println(b+ "is Largest Number");
+        }
+        else
+        {
+            System.out.println(c+ "is Largest Number");
+        }
+        
+    }
+}
