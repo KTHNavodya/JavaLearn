98564e544877febee076a0e41a37d2a3ecc81261
KTHNavodya <navodya43hiruni@gmail.com>
2024-11-29 19:34:27 +0530

    RandomMychange

--- src/Default/GenerateRandomNumAndString.java
+++ src/Default/GenerateRandomNumAndString.java
@@ -10,7 +10,10 @@ public class GenerateRandomNumAndString {
 
         Random rand=new Random();
 
-        int rand_int=rand.nextInt(100);
-        System.out.println(rand_int);
+        //int rand_int=rand.nextInt(100);
+        //System.out.println(rand_int);
+
+        double rand_dbl=rand.nextDouble(); //range 0.0 and less than 1.0
+        System.out.println(rand_dbl);
     }
 }
