e90bc1d4ae7b989f06f9d9dc6377ec29fcbedfd3
Hiruni Navodya <168626085+KTHNavodya@users.noreply.github.com>
2025-02-25 19:55:22 +0530

    changeArray

--- src/Default/ChackArraysEqual.java
+++ src/Default/ChackArraysEqual.java
@@ -5,7 +5,9 @@ public class CheckArraysEqual
         int a1[]={1,2,3,4,5};
         int a2[]={1,2,3,4,5};
 
-        boolean status=Arrays.equals(a1,a2);
+        //Appraoch1
+
+        /*boolean status=Arrays.equals(a1,a2);
 
         if(status==true)
         {
@@ -14,7 +16,29 @@ public class CheckArraysEqual
         else
         {
             System.out.println("Arrays are NOT Equal");
-        }
-    }
+        }*/
+
+       ////Appraoch2
+       boolean status = true;
+
+       if(a1.length == a2.length)
+           {
+            for(int i=0;i<a1.length;i++)
+            {
+                if(a1[i] != a2[i])
+                {
+                    status = false;
+                }
+            }
+           }
+           else{
+            status=false;
+           }
+           if(status==true)
+           }
+           else
+           {
+            System.out.println("Arrays are NOT Equal");
+           }
     
 }
